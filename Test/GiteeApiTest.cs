3499c8ec2364e894c15f53a8a04710550e52bd6f
Tim Cooper <timetickme@outlook.com>
2025-02-23 17:10:49 +0800

    完善测试

--- Test/GiteeApiTest.cs
+++ Test/GiteeApiTest.cs
@@ -9,6 +9,79 @@ namespace Test
 {
     public class GiteeApiTest
     {
+        [Fact(Skip = "跳过")]
+        public async Task TestRepoWorkFlow()
+        {
+            //先创建一个test仓库
+            //列出所有仓库
+            //清空一个仓库
+            //删除一个仓库
+
+            var token = Utils.GetToken();
+            Assert.Equal(32, token.Length);
+
+            var api = new GiteeApi(token);
+
+            var result = await api.GetAllRepos();
+            Assert.Equal(0, result.Code);
+            Assert.True(result.Data.Count > 10);
+
+            var repo = result.Data.First(r => r.Name == "test");
+            Assert.NotNull(repo);
+
+            var clearResult = await api.ClearRepo(repo.Name);
+            Assert.Equal(0, clearResult.Code);
+
+            var deleteResult = await api.DeleteRepo(repo.Name);
+            Assert.Equal(0, deleteResult.Code);
+        }
+
+        [Fact()]
+        public async Task TestGistsWorkflow()
+        {
+            //列出所有Gists
+            //创建一个Gist
+            //更新一个Gist
+            //删除一个Gist
+
+            var token = Utils.GetToken();
+            Assert.Equal(32, token.Length);
+
+            var api = new GiteeApi(token);
+            var title = "自动单元测试流程";
+            var fileContent = "自动测试文件内容";
+            var newFileContent = fileContent + $" update:{DateTime.Now}";
+
+            var filename = DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss");
+            var fullpath = Path.Combine(Environment.CurrentDirectory, filename);
+            await File.WriteAllTextAsync(fullpath, fileContent);
+
+            var createResult = await api.CreateGists(title, filename);
+            Assert.Equal(0, createResult.Code);
+
+            var result = await api.GetAllGists();
+            Assert.Equal(0, result.Code);
+            Assert.True(result.Data.Count > 0);
+
+            var gists = result.Data.First(g => g.Description == title);
+            Assert.NotNull(gists);
+
+            await File.WriteAllTextAsync(fullpath, newFileContent);
+
+            var updateResult = await api.UpdateGists(gists.Id, title, filename);
+            Assert.Equal(0, updateResult.Code);
+            File.Delete(fullpath);
+
+            var downloadResult = await api.GetGistsById(gists.Id);
+            Assert.Equal(0, downloadResult.Code);
+            Assert.NotNull(downloadResult.Data);
+
+            Assert.Equal(downloadResult.Data.Files[filename].Content, newFileContent);
+
+            var deleteResult = await api.DeleteGists(gists.Id);
+            Assert.Equal(0, deleteResult.Code);
+        }
+
         [Theory(Skip = "暂时禁用此测试")]
         [InlineData("git@gitee.com:imyinnan/test.git")]
         public async Task TestDeleteRepo(string url)
@@ -22,7 +95,7 @@ namespace Test
         }
 
         [Theory(Skip = "暂时禁用此测试")]
-        [InlineData("imyinnan/test")]
+        [InlineData("test")]
         public async Task TestClearRepo(string name)
         {
             var token = Utils.GetToken();
@@ -84,10 +157,9 @@ namespace Test
             var result = await api.GetAllGists();
 
             Assert.Equal(0, result.Code);
-            ///Assert.Equal("4a2fmjyczbidv9se0pk1r33", result.Data[0].Id);
         }
 
-        [Theory]
+        [Theory(Skip = "暂时禁用此测试")]
         [InlineData("测试片段", "test.txt")]
         public async Task TestCreateGist(string title, string file)
         {
